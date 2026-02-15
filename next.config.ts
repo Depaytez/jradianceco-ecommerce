149dc4a2ea71c32cbbc78f0df7c6b6a538581fbc
Oluwaseye Ayooluwa Philip <depaytez@gmail.com>
2026-02-15 20:35:29 +0100

    fix (next.config.ts): logo image display issue for deployment issue

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-   domains: ['www.jradianceco.com'],
   images: {
     remotePatterns: [
       {
@@ -10,12 +9,16 @@ const nextConfig: NextConfig = {
         hostname: "jradianceco.com",
         pathname: "/**",
       },
+      {
+        protocol: "https",
+        hostname: "www.jradianceco.com",
+        pathname: "/**",
+      },
     ],
     // image errors
     minimumCacheTTL: 60 * 60 * 24, // 24 hours
     deviceSizes: [640, 750, 828, 1080, 1200, 1920],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
-  
   },
 };
 
@@ -27,7 +30,7 @@ export async function headers() {
         {
           key: "Content-Security-Policy",
           value:
-            "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://js.paystack.co https://fp.paystack.co https://*.supabase.co https://*.fingerprintjs.com https://*.fpcdn.io https://*.datadoghq.com; style-src 'self' 'unsafe-inline'; img-src 'self' data: https:; connect-src 'self' https://*.supabase.co https://*.datadoghq.com https://fp.paystack.co; frame-src 'self' https://js.paystack.co;",
+            "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://js.paystack.co https://fp.paystack.co https://*.supabase.co https://*.fingerprintjs.com https://*.fpcdn.io https://*.datadoghq.com; style-src 'self' 'unsafe-inline'; img-src 'self' data: blob: https://jradianceco.com https://www.jradianceco.com; connect-src 'self' https://*.supabase.co https://*.datadoghq.com https://fp.paystack.co; frame-src 'self' https://js.paystack.co;",
         },
       ],
     },
